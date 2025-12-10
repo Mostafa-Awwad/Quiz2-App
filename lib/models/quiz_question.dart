7ed97d1aa738cbe9d61575445e0f823da4c3ffaf
Laboratory 07 <lab07@lab.mu.edu>
2025-12-10 15:41:16 +0200

    final submission

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,12 +1,9 @@
 class QuizQuestion {
-  const QuizQuestion(this.text, this.answers,this.shuffledAnswers);
   final List<String> shuffledAnswers;
-  final String text;
-  final List<String> answers;
 
-  QuizQuestion() : shuffledAnswers = List.of(answers)..shuffle();
+  QuizQuestion({
+     String? correctAnswer,
+     List<String>? answers,
+     String? text,
+  }) : shuffledAnswers = List.of(answers!)..shuffle();
 }
-  
-
-
-
